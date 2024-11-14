906e8239c77015e452c8ac3c5e2ba8ac8ee8c74e
vaibhav <kadamvaibhav90@yahoo.com>
2024-11-14 16:05:57 +0530

    new code added

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,10 +1,9 @@
 package com.driver;
 
 public class B extends A{
-    //override A
-@Override
-    public  void meth(){
-        System.out.println("Method is overridden in Extended class B");
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
     }
-}
 
+    }
